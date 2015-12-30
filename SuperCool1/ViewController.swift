4d32f7ab8d25ccafee65e2cf967f7be29c0287ab
bingqing <bing12123@hotmail.com>
2015-12-30 23:30:39 +0800

    Initial Commit

---
+++ SuperCool1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SuperCool1
+//
+//  Created by Li BingQing on 30/12/15.
+//  Copyright © 2015 BQ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
